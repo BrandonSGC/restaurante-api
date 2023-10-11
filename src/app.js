0991602503f824376fc11c7262672be8383b55f4
BrandonSGC <brangmz20@gmail.com>
2023-10-11 17:39:51 -0600

    Adding cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import platilloRoutes from './routes/platillos.routes.js';
 import categoriaRoutes from './routes/categorias.routes.js'; 
-import admplatilloroutes from './routes/adm.platillo.routes.js'; 
+import admplatilloroutes from './routes/adm.platillo.routes.js';
+import cors from 'cors'; // Importa el módulo 'cors'
+
 const app = express();
 
+// Middleware para habilitar CORS
+app.use(cors());
 // Middlewares
 app.use(express.json());
 
